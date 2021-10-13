902a780762b3f6010c0a5bd0b004a2ce598da904
Peter Kieltyka <peter.kieltyka@gmail.com>
2021-10-12 21:53:28 -0400

    update adapter js gen to es2020

--- src/gen/adapter/index.js
+++ src/gen/adapter/index.js
@@ -1,12 +1,1 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-exports.__esModule = true;
-exports.NiftyupNFT__factory = void 0;
-var NiftyupNFT__factory_1 = require("./factories/NiftyupNFT__factory");
-__createBinding(exports, NiftyupNFT__factory_1, "NiftyupNFT__factory");
+export { NiftyupNFT__factory } from "./factories/NiftyupNFT__factory";
