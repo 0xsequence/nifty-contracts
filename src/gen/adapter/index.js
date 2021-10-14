7fe6bcf985a9d28c60818780c6546749be0f88d9
Agustin Aguilar <Agusxrun@gmail.com>
2021-10-14 15:43:39 +0000

    Push commit and add precommit

--- src/gen/adapter/index.js
+++ src/gen/adapter/index.js
@@ -1,1 +1,5 @@
-export { NiftyupNFT__factory } from "./factories/NiftyupNFT__factory";
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NiftyupNFT__factory = void 0;
+var NiftyupNFT__factory_1 = require("./factories/NiftyupNFT__factory");
+Object.defineProperty(exports, "NiftyupNFT__factory", { enumerable: true, get: function () { return NiftyupNFT__factory_1.NiftyupNFT__factory; } });
